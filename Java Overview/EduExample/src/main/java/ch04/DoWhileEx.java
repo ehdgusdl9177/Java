d0ea181668d491c80eff4492f846a2eca1ac3dd7
ehdgusdl9177 <k23129177@gmail.com>
2022-11-11 13:52:36 +0900

    example

---
+++ Java Overview/EduExample/src/main/java/ch04/DoWhileEx.java
@@ -0,0 +1,11 @@
+package ch04;
+
+public class DoWhileEx {
+    public static void main(String[] args) {
+        int num = 0;
+        do {
+            System.out.println(num + " ");
+            num++;
+        } while(num < 3);
+    }
+}
